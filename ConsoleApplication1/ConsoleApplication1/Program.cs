e06219cc1d91606b5c43f766262ba13782c1d2be
Vladyslav Hryhorenko <grigorenkovlad1993@gmail.com>
2016-05-25 22:07:22 +0300

    2: add class with methods

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+static class Calculator
+{
+    public static double Sum(double x, double y)
+    {
+        return x + y;
+    }
+
+    public static double Dif(double x, double y)
+    {
+        return x - y;
+    }
+}
+
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Sum is:");
+            Console.WriteLine(Calculator.Sum(10,5));
+            Console.WriteLine("Difference is:");
+            Console.WriteLine(Calculator.Dif(10,5));
         }
     }
 }
